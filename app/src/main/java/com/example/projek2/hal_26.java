d1c07ff8e4785b415a636f480400a12d5c7bfd60
Zefanya <vendradevendra@gmail.com>
2021-07-29 07:15:29 +0700

    update ve : 1-29 done

--- app/src/main/java/com/example/projek2/hal_26.java
+++ app/src/main/java/com/example/projek2/hal_26.java
@@ -3,17 +3,12 @@ package com.example.projek2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 
-import java.io.Console;
-
-public class hal_1 extends AppCompatActivity {
+public class hal_26 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hal_1);
-
-        Log.d("test","Berhasil run");
+        setContentView(R.layout.activity_hal_26);
     }
 }
