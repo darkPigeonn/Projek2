9238d55be6ce140f894651d7de0d67fa6a11b860
Zefanya <vendradevendra@gmail.com>
2021-08-01 17:55:25 +0700

    update ve : 1-60 DONE

---
+++ app/src/main/java/com/example/projek2/hal_62.java
@@ -0,0 +1,14 @@
+package com.example.projek2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class hal_62 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hal_62);
+    }
+}
